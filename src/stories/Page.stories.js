ed126f30f65b79fc7b63a344d5d7162f7df912c9
fazulk <jfasulkey@gmail.com>
2022-09-08 21:23:22 -0700

    feat works

---
+++ src/stories/Page.stories.js
@@ -0,0 +1,37 @@
+import MyPage from './Page.vue';
+import * as HeaderStories from './Header.stories';
+import { within, userEvent } from '@storybook/testing-library';
+
+export default {
+  title: 'Example/Page',
+  component: MyPage,
+};
+
+const Template = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { MyPage },
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    // Story args can be mapped to keys in the returned object
+    return { user: args.user };
+  },
+  // Then, those values can be accessed directly in the template
+  template: '<my-page :user="user" />',
+});
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  ...HeaderStories.LoggedIn.args,
+};
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {
+  ...HeaderStories.LoggedOut.args,
+};
+
+
+LoggedIn.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  const loginButton = await canvas.getByRole('button', { name: /Log in/i });
+  await userEvent.click(loginButton);
+};
